d7952529045325730eeb60a960ef716b3a2f1824
Eero Anttila <eea@iki.fi>
2009-04-20 09:16:02 +0000

    works also with changhing months

--- src/continuous-calendar-test.js
+++ src/continuous-calendar-test.js
@@ -3,19 +3,19 @@ module("Charge from customer selector", {
   tearDown: resetAll
 });
 
-test("lists week days for 18.4.2009", function() {
-  $("#continuousCalendar").continuousCalendar(18, 4, 2009);
+test("lists week days for vappu 2009", function() {
+  $("#continuousCalendar").continuousCalendar(30, 4, 2009);
   equals($("#continuousCalendar").html(), [
     "<table>",
     "<tbody>",
     "<tr>",
-    "<td>13</td>",
-    "<td>14</td>",
-    "<td>15</td>",
-    "<td>16</td>",
-    "<td>17</td>",
-    "<td>18</td>",
-    "<td>19</td>",
+    "<td>27</td>",
+    "<td>28</td>",
+    "<td>29</td>",
+    "<td>30</td>",
+    "<td>1</td>",
+    "<td>2</td>",
+    "<td>3</td>",
     "</tr>",
     "</tbody>",
     "</table>"
