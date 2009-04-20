2b6ca0fa29cf9f9c8b52dfa24d1bca20e37def60
Eero Anttila <eea@iki.fi>
2009-04-20 09:10:50 +0000

    lists dates of one week

--- src/continuous-calendar-test.js
+++ src/continuous-calendar-test.js
@@ -5,12 +5,27 @@ module("Charge from customer selector", {
 test("lists week days for 18.4.2009", function() {
   var specifiedDay = new Date("4/18/2009");
   var monday = specifiedDay.getFirstDateOfWeek(1);
-  equals(monday.getDate(),13);
+  equals(monday.getDate(), 13);
   var sunday = monday.plusDays(6);
   equals(sunday.getDate(), 19);
 
-  $("#continuousCalendar").continuousCalendar(18,4,2009);
-  equals($("#continuousCalendar").html(), '<table><tbody><tr><td>13</td></tr></tbody></table>');
+  $("#continuousCalendar").continuousCalendar(18, 4, 2009);
+  equals($("#continuousCalendar").html(), [
+    "<table>",
+    "<tbody>",
+    "<tr>",
+    "<td>13</td>",
+    "<td>14</td>",
+    "<td>15</td>",
+    "<td>16</td>",
+    "<td>17</td>",
+    "<td>18</td>",
+    "<td>19</td>",
+    "</tr>",
+    "</tbody>",
+    "</table>"
+  ].join("\n"));
+
   //equals($("#continuousCalendar").html(), "<table><tr><td>13</td><td>14</td><td>1</td><td>1</td><td>1</td>");
   //same([1,2,3],[13,14,15,16,17,18,19]);
   //x.getDate() //18
--- src/continuous-calendar.js
+++ src/continuous-calendar.js
@@ -1,5 +1,17 @@
 $.fn.continuousCalendar = function(day, month, year) {
   var date = new Date(month + "/" + day + "/" + year);
   var monday = date.getFirstDateOfWeek(1);
-  this.append("<table><tbody><tr><td>" + monday.getDate() + "</td></tr></tbody></table>");
+  var weekMarkup = [];
+  for (var i = 0; i < 7; i++) {
+    weekMarkup.push("<td>" + monday.plusDays(i).getDate() + "</td>");
+  }
+  this.append([
+    "<table>",
+    "<tbody>",
+    "<tr>",
+    weekMarkup.join("\n"),
+    "</tr>",
+    "</tbody>",
+    "</table>"
+  ].join("\n"));
 }
