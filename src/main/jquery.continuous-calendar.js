72c4e34038edc612151a8f396ba3850538dadc2f
Eero Anttila <eea@iki.fi>
2011-03-09 15:15:38 +0200

    add semicolon at start to prevent compile errors

--- src/main/jquery.continuous-calendar.js
+++ src/main/jquery.continuous-calendar.js
@@ -11,7 +11,7 @@
  * License for the specific language governing permissions and limitations under
  * the License.
  */
-(function($) {
+;(function($) {
   $.fn.continuousCalendar = function(options) {
     this.each(function() {
       _continuousCalendar.call($(this), options)
