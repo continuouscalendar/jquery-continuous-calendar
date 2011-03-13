86a8db2449a71959e661d754c16fc907905741f7
Eero Anttila <eea@iki.fi>
2011-03-13 19:34:10 +0200

    update build script

--- build.sh
+++ build.sh
@@ -1,12 +1,16 @@
 #!/bin/bash
 current_dir=$(pwd)
 cd $(dirname $0)
-echo Deleting previous build...
-rm -rf build
 version=$(git tag | head)
-echo Build version $version
-mkdir build
-find src/main -name *.js |xargs cat >build/jquery.continuous-calendar-$version.js
-cp build/jquery.continuous-calendar-$version.js build/jquery.continuous-calendar-latest.js
-cp src/main/*.css build/jquery.continuous-calendar-$version.css
+echo Build nightly...
+find src/main -name *.js |xargs cat >build/jquery.continuous-calendar-latest.js
+cp src/main/jquery.continuous-calendar.css build/jquery.continuous-calendar-latest.css
+if [ -f build/jquery.continuous-calendar-$version.js ]
+then
+	echo "Version $version exists, no new versions. create by typing git tag <version>"
+else
+	echo "Creating version $version"
+	cp build/jquery.continuous-calendar-latest.js build/jquery.continuous-calendar-$version.js
+	cp build/jquery.continuous-calendar-latest.css build/jquery.continuous-calendar-$version.css 	
+fi
 cd $current_dir
