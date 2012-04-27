a7fb409c206a45ba32e989f4116fabbb08f3f6d2
Eero Anttila <eea@iki.fi>
2012-04-27 22:27:39 +0300

    Rename date-range to DateRange

--- build/jquery.continuous-calendar-latest.js
+++ build/jquery.continuous-calendar-latest.js
@@ -1,4 +1,4 @@
-$.continuousCalendar = {};$.continuousCalendar.version = '1.1.5';$.continuousCalendar.released = '2012-05-04'
+$.continuousCalendar = {};$.continuousCalendar.version = '';$.continuousCalendar.released = '2012-05-04'
 /* ==============================================================================
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not
  * use this file except in compliance with the License. You may obtain a copy of
