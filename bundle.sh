c30444197d7c1546035f15ef2b1063665071a194
Eero Anttila <eea@iki.fi>
2012-09-06 22:38:54 +0300

    Add example for minifying using r.js

---
+++ bundle.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+#java -classpath src/lib/js.jar org.mozilla.javascript.tools.shell.Main src/lib/r-2.1.2.js src/main/build.js optimize=none
+#node  src/lib/r-2.1.2.js -o src/main/build.js optimize=none
+node  src/lib/r-2.1.2.js -o src/main/build.js
---
+++ src/main/build.js
@@ -0,0 +1,11 @@
+({
+  appDir        : "",
+  baseUrl       : ".",
+  name          : 'jquery.continuousCalendar',
+  out           : '../../build/jquery.continuousCalendar-latest.js',
+  urlArgs       : undefined,
+  paths: {
+    jquery : 'empty:',
+    'jquery.tinyscrollbar': 'jquery.tinyscrollbar-1.66/jquery.tinyscrollbar'
+  }
+})
