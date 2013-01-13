0ccac3dd748a8762d025dbbc19b0529b1a82c11a
Eero Anttila <eero.anttila@reaktor.fi>
2013-01-13 18:33:07 +0200

    Use almond for doing bundle for index.html

--- src/main/build.js
+++ src/main/build.js
@@ -1,11 +1,17 @@
 ({
-  appDir        : "",
-  baseUrl       : ".",
-  name          : 'jquery.continuousCalendar',
-  out           : '../../build/jquery.continuousCalendar-latest.js',
-  urlArgs       : undefined,
-  paths: {
-    jquery : 'empty:',
+  appDir  : "",
+  baseUrl : ".",
+  name    : '../lib/almond',
+  include: ['jquery.continuousCalendar'],
+  insertRequire: ['jquery.continuousCalendar'],
+  out     : '../../build/jquery.continuousCalendar-latest.js',
+  urlArgs : undefined,
+  paths   : {
+    jquery                : 'jqueryStub',
     'jquery.tinyscrollbar': 'jquery.tinyscrollbar-1.66/jquery.tinyscrollbar'
+  },
+  wrap:{
+    startFile:'wrapStart.js',
+    endFile:'wrapEnd.js'
   }
 })
