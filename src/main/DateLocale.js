b4437ccd6b672ad4e77a634cd7b7872efc991aba
Eero Anttila <eea@iki.fi>
2013-02-17 00:32:34 +0200

    Move date locales into object

--- src/main/DateLocale.js
+++ src/main/DateLocale.js
@@ -6,114 +6,109 @@
     root.DateLocale = factory(root.DateTime, root.DateFormat)
   }
 })(this, function(DateTime, DateFormat) {
-  var DateLocale = {}
-
-  DateLocale.FI = {
-    id             : 'FI',
-    monthNames     : ['tammikuu', 'helmikuu', 'maaliskuu', 'huhtikuu', 'toukokuu', 'kesäkuu', 'heinäkuu', 'elokuu', 'syyskuu', 'lokakuu', 'marraskuu', 'joulukuu'],
-    dayNames       : ['sunnuntai', 'maanantai', 'tiistai', 'keskiviikko', 'torstai', 'perjantai', 'lauantai'],
-    shortDayNames  : ['su', 'ma', 'ti', 'ke', 'to', 'pe', 'la'],
-    yearsLabel     : function(years) { return years + ' ' + (years == '1' ? 'vuosi' : 'vuotta') },
-    monthsLabel    : function(months) { return months + ' ' + (months == '1' ? 'kuukausi' : 'kuukautta') },
-    daysLabel      : function(days) { return days + ' ' + (days == '1' ? 'päivä' : 'päivää') },
-    hoursLabel     : function(hours, minutes) {
-      var hoursAndMinutes = DateFormat.hoursAndMinutes(hours, minutes).replace('.', ',')
-      return hoursAndMinutes + ' ' + (hoursAndMinutes == '1' ? 'tunti' : 'tuntia')
+  var DateLocale = {
+    FI: {
+      id             : 'FI',
+      monthNames     : ['tammikuu', 'helmikuu', 'maaliskuu', 'huhtikuu', 'toukokuu', 'kesäkuu', 'heinäkuu', 'elokuu', 'syyskuu', 'lokakuu', 'marraskuu', 'joulukuu'],
+      dayNames       : ['sunnuntai', 'maanantai', 'tiistai', 'keskiviikko', 'torstai', 'perjantai', 'lauantai'],
+      shortDayNames  : ['su', 'ma', 'ti', 'ke', 'to', 'pe', 'la'],
+      yearsLabel     : function(years) { return years + ' ' + (years == '1' ? 'vuosi' : 'vuotta') },
+      monthsLabel    : function(months) { return months + ' ' + (months == '1' ? 'kuukausi' : 'kuukautta') },
+      daysLabel      : function(days) { return days + ' ' + (days == '1' ? 'päivä' : 'päivää') },
+      hoursLabel     : function(hours, minutes) {
+        var hoursAndMinutes = DateFormat.hoursAndMinutes(hours, minutes).replace('.', ',')
+        return hoursAndMinutes + ' ' + (hoursAndMinutes == '1' ? 'tunti' : 'tuntia')
+      },
+      shortDateFormat: 'j.n.Y',
+      weekDateFormat : 'D j.n.Y',
+      dateTimeFormat : 'D j.n.Y k\\lo G:i',
+      firstWeekday   : DateTime.MONDAY
     },
-    shortDateFormat: 'j.n.Y',
-    weekDateFormat : 'D j.n.Y',
-    dateTimeFormat : 'D j.n.Y k\\lo G:i',
-    firstWeekday   : DateTime.MONDAY
-  }
-
-  DateLocale.EN = {
-    id             : 'EN',
-    monthNames     : ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'],
-    dayNames       : ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'],
-    shortDayNames  : ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'],
-    yearsLabel     : function(years) { return years + ' ' + (years == '1' ? 'Year' : 'Years'); },
-    monthsLabel    : function(months) { return months + ' ' + (months == '1' ? 'Months' : 'Months') },
-    daysLabel      : function(days) { return days + ' ' + (days == '1' ? 'Day' : 'Days') },
-    hoursLabel     : function(hours, minutes) {
-      var hoursAndMinutes = DateFormat.hoursAndMinutes(hours, minutes)
-      return hoursAndMinutes + ' ' + (hoursAndMinutes == '1' ? 'Hour' : 'Hours')
+    EN: {
+      id             : 'EN',
+      monthNames     : ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'],
+      dayNames       : ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'],
+      shortDayNames  : ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'],
+      yearsLabel     : function(years) { return years + ' ' + (years == '1' ? 'Year' : 'Years'); },
+      monthsLabel    : function(months) { return months + ' ' + (months == '1' ? 'Months' : 'Months') },
+      daysLabel      : function(days) { return days + ' ' + (days == '1' ? 'Day' : 'Days') },
+      hoursLabel     : function(hours, minutes) {
+        var hoursAndMinutes = DateFormat.hoursAndMinutes(hours, minutes)
+        return hoursAndMinutes + ' ' + (hoursAndMinutes == '1' ? 'Hour' : 'Hours')
+      },
+      shortDateFormat: 'n/j/Y',
+      weekDateFormat : 'D n/j/Y',
+      dateTimeFormat : 'D n/j/Y G:i',
+      firstWeekday   : DateTime.SUNDAY
     },
-    shortDateFormat: 'n/j/Y',
-    weekDateFormat : 'D n/j/Y',
-    dateTimeFormat : 'D n/j/Y G:i',
-    firstWeekday   : DateTime.SUNDAY
-  }
-
-  DateLocale.AU = {
-    id             : 'AU',
-    monthNames     : ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'],
-    dayNames       : ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'],
-    shortDayNames  : ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'],
-    yearsLabel     : function(years) { return years + ' ' + (years == '1' ? 'Year' : 'Years'); },
-    monthsLabel    : function(months) { return months + ' ' + (months == '1' ? 'Months' : 'Months') },
-    daysLabel      : function(days) { return days + ' ' + (days == '1' ? 'Day' : 'Days') },
-    hoursLabel     : function(hours, minutes) {
-      var hoursAndMinutes = DateFormat.hoursAndMinutes(hours, minutes)
-      return hoursAndMinutes + ' ' + (hoursAndMinutes == '1' ? 'Hour' : 'Hours')
+    AU: {
+      id             : 'AU',
+      monthNames     : ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'],
+      dayNames       : ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'],
+      shortDayNames  : ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'],
+      yearsLabel     : function(years) { return years + ' ' + (years == '1' ? 'Year' : 'Years'); },
+      monthsLabel    : function(months) { return months + ' ' + (months == '1' ? 'Months' : 'Months') },
+      daysLabel      : function(days) { return days + ' ' + (days == '1' ? 'Day' : 'Days') },
+      hoursLabel     : function(hours, minutes) {
+        var hoursAndMinutes = DateFormat.hoursAndMinutes(hours, minutes)
+        return hoursAndMinutes + ' ' + (hoursAndMinutes == '1' ? 'Hour' : 'Hours')
+      },
+      shortDateFormat: 'j/n/Y',
+      weekDateFormat : 'D j/n/Y',
+      dateTimeFormat : 'D j/n/Y G:i',
+      firstWeekday   : DateTime.SUNDAY
     },
-    shortDateFormat: 'j/n/Y',
-    weekDateFormat : 'D j/n/Y',
-    dateTimeFormat : 'D j/n/Y G:i',
-    firstWeekday   : DateTime.SUNDAY
-  }
-
-  DateLocale.ET = {
-    id             : 'ET',
-    monthNames     : [ 'Jaanuar', 'Veebruar', 'Märts', 'Aprill', 'Mai', 'Juuni', 'Juuli', 'August', 'September', 'Oktoober', 'November', 'Detsember'],
-    dayNames       : ['Pühapäev', 'Esmaspäev', 'Teisipäev', 'Kolmapäev', 'Neljapäev', 'Reede', 'Laupäev'],
-    shortDayNames  : ['Pü', 'Es', 'Te', 'Ko', 'Ne', 'Re', 'La'],
-    yearsLabel     : function(years) { return years + ' ' + (years == '1' ? 'Aasta' : 'Aastat') },
-    monthsLabel    : function(months) { return months + ' ' + (months == '1' ? 'Kuu' : 'Kuud') },
-    daysLabel      : function(days) { return days + ' ' + (days == '1' ? 'Päev' : 'Päeva') },
-    hoursLabel     : function(hours, minutes) {
-      var hoursAndMinutes = DateFormat.hoursAndMinutes(hours, minutes).replace('.', ',')
-      return hoursAndMinutes + ' ' + (hoursAndMinutes == '1' ? 'Tund' : 'Tundi')
+    ET: {
+      id             : 'ET',
+      monthNames     : [ 'Jaanuar', 'Veebruar', 'Märts', 'Aprill', 'Mai', 'Juuni', 'Juuli', 'August', 'September', 'Oktoober', 'November', 'Detsember'],
+      dayNames       : ['Pühapäev', 'Esmaspäev', 'Teisipäev', 'Kolmapäev', 'Neljapäev', 'Reede', 'Laupäev'],
+      shortDayNames  : ['Pü', 'Es', 'Te', 'Ko', 'Ne', 'Re', 'La'],
+      yearsLabel     : function(years) { return years + ' ' + (years == '1' ? 'Aasta' : 'Aastat') },
+      monthsLabel    : function(months) { return months + ' ' + (months == '1' ? 'Kuu' : 'Kuud') },
+      daysLabel      : function(days) { return days + ' ' + (days == '1' ? 'Päev' : 'Päeva') },
+      hoursLabel     : function(hours, minutes) {
+        var hoursAndMinutes = DateFormat.hoursAndMinutes(hours, minutes).replace('.', ',')
+        return hoursAndMinutes + ' ' + (hoursAndMinutes == '1' ? 'Tund' : 'Tundi')
+      },
+      shortDateFormat: 'j.n.Y',
+      weekDateFormat : 'D j.n.Y',
+      dateTimeFormat : 'D j.n.Y k\\lo G:i',
+      firstWeekday   : DateTime.MONDAY
     },
-    shortDateFormat: 'j.n.Y',
-    weekDateFormat : 'D j.n.Y',
-    dateTimeFormat : 'D j.n.Y k\\lo G:i',
-    firstWeekday   : DateTime.MONDAY
-  }
-
-  DateLocale.RU = {
-    id             : 'RU',
-    monthNames     : [ 'Январь', 'Февраль', 'Март', 'Апрель', 'Май', 'Июнь', 'Июль', 'Август', 'Сентябрь', 'Октябрь', 'Ноябрь', 'Декабрь'],
-    dayNames       : ['Воскресенье', 'Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота'],
-    shortDayNames  : ['Вс', 'Пн', 'Вт', 'Ср', 'Чт', 'Пт', 'Сб'],
-    yearsLabel     : function(years) { return years + ' ' + (years == '1' ? 'Год' : 'Года') },
-    monthsLabel    : function(months) { return months + ' ' + (months == '1' ? 'Месяц' : 'Месяца') },
-    daysLabel      : function(days) { return days + ' ' + (days == '1' ? 'День' : 'Дня') },
-    hoursLabel     : function(hours, minutes) {
-      var hoursAndMinutes = DateFormat.hoursAndMinutes(hours, minutes).replace('.', ',')
-      return hoursAndMinutes + ' ' + (hoursAndMinutes == '1' ? 'Минута' : 'Минуты')
+    RU: {
+      id             : 'RU',
+      monthNames     : [ 'Январь', 'Февраль', 'Март', 'Апрель', 'Май', 'Июнь', 'Июль', 'Август', 'Сентябрь', 'Октябрь', 'Ноябрь', 'Декабрь'],
+      dayNames       : ['Воскресенье', 'Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота'],
+      shortDayNames  : ['Вс', 'Пн', 'Вт', 'Ср', 'Чт', 'Пт', 'Сб'],
+      yearsLabel     : function(years) { return years + ' ' + (years == '1' ? 'Год' : 'Года') },
+      monthsLabel    : function(months) { return months + ' ' + (months == '1' ? 'Месяц' : 'Месяца') },
+      daysLabel      : function(days) { return days + ' ' + (days == '1' ? 'День' : 'Дня') },
+      hoursLabel     : function(hours, minutes) {
+        var hoursAndMinutes = DateFormat.hoursAndMinutes(hours, minutes).replace('.', ',')
+        return hoursAndMinutes + ' ' + (hoursAndMinutes == '1' ? 'Минута' : 'Минуты')
+      },
+      shortDateFormat: 'j.n.Y',
+      weekDateFormat : 'D j.n.Y',
+      dateTimeFormat : 'D j.n.Y k\\lo G:i',
+      firstWeekday   : DateTime.MONDAY
     },
-    shortDateFormat: 'j.n.Y',
-    weekDateFormat : 'D j.n.Y',
-    dateTimeFormat : 'D j.n.Y k\\lo G:i',
-    firstWeekday   : DateTime.MONDAY
-  }
-
-  DateLocale.SV = {
-    id             : 'SV',
-    monthNames     : ['Januari', 'Februari', 'Mars', 'April', 'Maj', 'Juni', 'Juli', 'Augusti', 'September', 'Oktober', 'November', 'December'],
-    dayNames       : ['Söndag', 'Måndag', 'Tisdag', 'Onsdag', 'Torsdag', 'Fredag', 'Lördag'],
-    shortDayNames  : ['Sö', 'Må', 'Ti', 'On', 'To', 'Fr', 'Lö'],
-    yearsLabel     : function(years) { return years + ' ' + (years == '1' ? 'År' : 'År') },
-    monthsLabel    : function(months) { return months + ' ' + (months == '1' ? 'Månad' : 'Månader') },
-    daysLabel      : function(days) { return days + ' ' + (days == '1' ? 'Dag' : 'Dagar') },
-    hoursLabel     : function(hours, minutes) {
-      var hoursAndMinutes = DateFormat.hoursAndMinutes(hours, minutes).replace('.', ',')
-      return hoursAndMinutes + ' ' + (hoursAndMinutes == '1' ? 'Minut' : 'Minuter')
-    },
-    shortDateFormat: 'j.n.Y',
-    weekDateFormat : 'D j.n.Y',
-    dateTimeFormat : 'D j.n.Y k\\lo G:i',
-    firstWeekday   : DateTime.MONDAY
+    SV: {
+      id             : 'SV',
+      monthNames     : ['Januari', 'Februari', 'Mars', 'April', 'Maj', 'Juni', 'Juli', 'Augusti', 'September', 'Oktober', 'November', 'December'],
+      dayNames       : ['Söndag', 'Måndag', 'Tisdag', 'Onsdag', 'Torsdag', 'Fredag', 'Lördag'],
+      shortDayNames  : ['Sö', 'Må', 'Ti', 'On', 'To', 'Fr', 'Lö'],
+      yearsLabel     : function(years) { return years + ' ' + (years == '1' ? 'År' : 'År') },
+      monthsLabel    : function(months) { return months + ' ' + (months == '1' ? 'Månad' : 'Månader') },
+      daysLabel      : function(days) { return days + ' ' + (days == '1' ? 'Dag' : 'Dagar') },
+      hoursLabel     : function(hours, minutes) {
+        var hoursAndMinutes = DateFormat.hoursAndMinutes(hours, minutes).replace('.', ',')
+        return hoursAndMinutes + ' ' + (hoursAndMinutes == '1' ? 'Minut' : 'Minuter')
+      },
+      shortDateFormat: 'j.n.Y',
+      weekDateFormat : 'D j.n.Y',
+      dateTimeFormat : 'D j.n.Y k\\lo G:i',
+      firstWeekday   : DateTime.MONDAY
+    }
   }
 
   DateLocale.fromArgument = function(stringOrObject) {
