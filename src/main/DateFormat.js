30187257e5db6e7c07c7801c5ea19739a965a84a
Eero Anttila <eea@iki.fi>
2013-02-17 00:13:56 +0200

    Remove license boilerplate from individual files

--- src/main/DateFormat.js
+++ src/main/DateFormat.js
@@ -1,16 +1,3 @@
-/*
- * Copyright (C) 2004 Baron Schwartz <baron at sequent dot org>
- *
- * This program is free software; you can redistribute it and/or modify it
- * under the terms of the GNU Lesser General Public License as published by the
- * Free Software Foundation, version 2.1.
- *
- * This program is distributed in the hope that it will be useful, but WITHOUT
- * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
- * FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public License for more
- * details.
- */
-
 ;
 (function(root, factory) {
   if(typeof define === "function" && define.amd) {
