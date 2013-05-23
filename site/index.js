afe8b3da48480a710762997fbcc949de8d908750
Eero Anttila <eea@iki.fi>
2013-05-23 15:11:41 +0300

    Remove inline script from index.html

---
+++ site/index.js
@@ -0,0 +1,18 @@
+$("#timeCalendar").continuousCalendar({weeksBefore: 30, lastDate: "today", selectToday: true, theme: 'rounded'});
+$("#timeCalendar").bind('calendarChange', function() {
+  var container = $(this);
+  var startTime = container.find('select[name=tripStartTime]').val();
+  var endTime = container.find('select[name=tripEndTime]').val();
+  var range = container.calendarRange();
+  range = range.withTimes(startTime, endTime);
+  container.find('.totalTimeOfTrip').text(DateFormat.formatRange(range, DateLocale.EN)).toggleClass('invalid', !range.isValid());
+});
+$("#timeCalendar select").bind('change', function() {
+  $("#timeCalendar").trigger('calendarChange');
+});
+$("#timeCalendar select").each(function() {
+  for(i = 0; i < 24; i++) {
+    $(this).append($("<option>").text(i + ":00")).append($("<option>").text(i + ":30"));
+  }
+});
+$('#date-picker').continuousCalendar({isPopup: true, firstDate: 'today', customScroll: true});
