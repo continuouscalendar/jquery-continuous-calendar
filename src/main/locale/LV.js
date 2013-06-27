71327654d7e05613beb791306a6a2be857a95880
Oleg Tsvetkov <olecve@gmail.com>
2013-06-27 17:25:44 +0300

    Add Latvian translation

---
+++ src/main/locale/LV.js
@@ -0,0 +1,21 @@
+define(function(require) {
+  var DateTime = require('../DateTime')
+  var DateFormat = require('../DateFormat')
+  return {
+    id             : 'LV',
+    monthNames     : ['Janvāris', 'Februāris', 'Marts', 'Aprīlis', 'Maijs', 'Jūnijs', ' Jūlijs', 'Augusts', 'Septembris', 'Oktobris', 'Novembris', 'Decembris'],
+    dayNames       : ['Svētdiena', ' Pirmdiena', 'Otrdiena', 'Trešdiena', 'Ceturtdiena', 'Piektdiäna', 'Sestdiena'],
+    shortDayNames  : ['Svē', 'Pir', 'Otr', 'Tre', 'Cet', 'Pie', 'Ses'],
+    yearsLabel     : function(years) { return years + ' ' + (years == '1' ? 'G' : 'G'); },
+    monthsLabel    : function(months) { return months + ' ' + (months == '1' ? 'Mēnesī' : 'Mēnešiem') },
+    daysLabel      : function(days) { return days + ' ' + (days == '1' ? 'Diena' : 'Dienas') },
+    hoursLabel     : function(hours, minutes) {
+      var hoursAndMinutes = DateFormat.hoursAndMinutes(hours, minutes)
+      return hoursAndMinutes + ' ' + (hoursAndMinutes == '1' ? 'Stundas' : 'Minūtes')
+    },
+    shortDateFormat: 'j.n.Y',
+    weekDateFormat : 'D j.n.Y',
+    dateTimeFormat : 'D j.n.Y k\\lo G:i',
+    firstWeekday   : DateTime.MONDAY
+  }
+})
