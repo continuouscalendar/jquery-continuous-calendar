8951627c07fcbd9f461b90f5fb10643102e16935
Maxim Chervonny <fiskus@chervonny.ru>
2013-09-12 00:30:46 +0400

    instead of equating properties of object, initialize whole object with those properties inside

--- src/main/Template.js
+++ src/main/Template.js
@@ -29,135 +29,135 @@
  * @return {Object.<Function.<String>>}
  */
 define(function(require) {
-  var templates = {}
+  var templates = {
+    header: function() { return helper('<table class="calendarHeader" />') },
 
-  templates.header = function() { return helper('<table class="calendarHeader" />') }
+    body: function() { return helper('<table class="calendarBody" />') },
 
-  templates.body = function() { return helper('<table class="calendarBody" />') }
+    scrollContent: function() { return helper('<div class="calendarScrollContent" />') },
 
-  templates.scrollContent = function() { return helper('<div class="calendarScrollContent" />') }
+    /**
+     * @private
+     */
+    _tinyScrollbar: function() {
+      var tmpl = [
+        '<div class="tinyscrollbar">',
+        '<div class="scrollbar">',
+        '<div class="track">',
+        '<div class="thumb">',
+        '<div class="end">',
+        '</div></div></div></div></div>'
+      ].join('')
+      return helper(tmpl)
+    },
 
-  /**
-   * @private
-   */
-  templates._tinyScrollbar = function() {
-    var tmpl = [
-      '<div class="tinyscrollbar">',
-      '<div class="scrollbar">',
-      '<div class="track">',
-      '<div class="thumb">',
-      '<div class="end">',
-      '</div></div></div></div></div>'
-    ].join('')
-    return helper(tmpl)
+    /**
+     * @private
+     */
+    _headerRow: function() { return helper('<tr><th class="month" /><th class="week">&nbsp;</th>') },
+
+    th: function() { return helper('<th>') },
+
+    thead: function() { return helper('<thead>') },
+
+    /**
+     * @param {object} data
+     * @config {string} rows
+     */
+    tbody: function(data) {
+      var tmpl = '<tbody><%= rows %></tbody>'
+      return helper(tmpl, data)
+    },
+
+    /**
+     * @param {object} data
+     * @config {string} content
+     */
+    bodyRow: function(data) {
+      var tmpl = '<tr><%= content %></tr>'
+      return helper(tmpl, data)
+    },
+
+    /**
+     * @private
+     * @param {object} data
+     * @config {string} index
+     * @config {string} classNames
+     * @config {string} content
+     */
+    _dateCell: function(data) {
+      var tmpl = [
+        '<td class="<%= classNames %>" date-cell-index="<%= index %>">',
+        '<%= content %>',
+        '</td>'
+      ].join('')
+      return helper(tmpl, data)
+    },
+
+    /**
+     * @param {object} data
+     * @config {string} classNames
+     * @config {string} content
+     */
+    weekCell: function(data) {
+      var tmpl = '<th class="week <%= classNames %>"><%= content %></th>'
+      return helper(tmpl, data)
+    },
+
+    /**
+     * @private
+     * @param {object} data
+     * @config {string} classNames
+     * @config {string} content
+     */
+    _monthCell: function(data) {
+      var tmpl = '<th class="month <%= classNames %>"><%= content %></th>'
+      return helper(tmpl, data)
+    },
+
+    innerWrapper: function() { return helper('<div />') },
+
+    /**
+     * @private
+     */
+    _lengthLabel: function() { return helper('<div class="label"><span class="rangeLengthLabel" /></div>') },
+
+    /**
+     * @private
+     */
+    _separator: function() { return helper('<span class="separator"> - </span>') },
+
+    /**
+     * @private
+     */
+    _endDateLabel: function() { return helper('<span class="endDateLabel" />') },
+
+    /**
+     * @private
+     */
+    _startDateLabel: function() { return helper('<div class="label"><span class="startDateLabel" /></div>') },
+
+    /**
+     * @private
+     */
+    _clearDates: function() { return helper('<span class="clearDates clickable" />') },
+
+    clearLabel: function() { return helper('<div class="label clearLabel" />') },
+
+    icon: function(data) {
+      var tmpl = '<a href="javascirpt:;" class="calendarIcon"><%= content %></a>'
+      return helper(tmpl, data)
+    },
+
+    popupContainer: function() { return helper('<div class="popUpContainer" />') },
+
+    /**
+     * @private
+     */
+    _calendar: function() { return helper('<div class="continuousCalendar" />') },
+
+    emptyContainer: function() { return helper('<div />') }
   }
-
-  /**
-   * @private
-   */
-  templates._headerRow = function() { return helper('<tr><th class="month" /><th class="week">&nbsp;</th>') }
-
-  templates.th = function() { return helper('<th>') }
-
-  templates.thead = function() { return helper('<thead>') }
-
-  /**
-   * @param {object} data
-   * @config {string} rows
-   */
-  templates.tbody = function(data) {
-    var tmpl = '<tbody><%= rows %></tbody>'
-    return helper(tmpl, data)
-  }
-
-  /**
-   * @param {object} data
-   * @config {string} content
-   */
-  templates.bodyRow = function(data) {
-    var tmpl = '<tr><%= content %></tr>'
-    return helper(tmpl, data)
-  }
-
-  /**
-   * @private
-   * @param {object} data
-   * @config {string} index
-   * @config {string} classNames
-   * @config {string} content
-   */
-  templates._dateCell = function(data) {
-    var tmpl = [
-      '<td class="<%= classNames %>" date-cell-index="<%= index %>">',
-      '<%= content %>',
-      '</td>'
-    ].join('')
-    return helper(tmpl, data)
-  }
-
-  /**
-   * @param {object} data
-   * @config {string} classNames
-   * @config {string} content
-   */
-  templates.weekCell = function(data) {
-    var tmpl = '<th class="week <%= classNames %>"><%= content %></th>'
-    return helper(tmpl, data)
-  }
-
-  /**
-   * @private
-   * @param {object} data
-   * @config {string} classNames
-   * @config {string} content
-   */
-  templates._monthCell = function(data) {
-    var tmpl = '<th class="month <%= classNames %>"><%= content %></th>'
-    return helper(tmpl, data)
-  }
-
-  templates.innerWrapper = function() { return helper('<div />') }
-
-  /**
-   * @private
-   */
-  templates._lengthLabel = function() { return helper('<div class="label"><span class="rangeLengthLabel" /></div>') }
-
-  /**
-   * @private
-   */
-  templates._separator = function() { return helper('<span class="separator"> - </span>') }
-
-  /**
-   * @private
-   */
-  templates._endDateLabel = function() { return helper('<span class="endDateLabel" />') }
-
-  /**
-   * @private
-   */
-  templates._startDateLabel = function() { return helper('<div class="label"><span class="startDateLabel" /></div>') }
-
-  /**
-   * @private
-   */
-  templates._clearDates = function() { return helper('<span class="clearDates clickable" />') }
-
-  templates.clearLabel = function() { return helper('<div class="label clearLabel" />') }
-
-  templates.icon = function(data) {
-    var tmpl = '<a href="javascirpt:;" class="calendarIcon"><%= content %></a>'
-    return helper(tmpl, data)
-  }
-
-  templates.popupContainer = function() { return helper('<div class="popUpContainer" />') }
-
-  /**
-   * @private
-   */
-  templates._calendar = function() { return helper('<div class="continuousCalendar" />') }
-
-  templates.emptyContainer = function() { return helper('<div />') }
 
   /**
    * @param {string} tmpl Should contain <%= `name` %> which replaced by `value`
