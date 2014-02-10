d8fda498f5f3fbbca1f5d536086b4765b640cefe
Eero Anttila <eero.anttila@reaktor.fi>
2014-02-10 22:50:07 +0200

    Remove redundant files

--- bundle.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-#java -classpath src/lib/js.jar org.mozilla.javascript.tools.shell.Main src/lib/r-2.1.2.js src/main/build.js optimize=none
-#node  src/lib/r-2.1.2.js -o src/main/build.js optimize=none
-#node  src/lib/r-2.1.2.js -o src/main/build.js
-node src/lib/r-2.1.2.js -o src/main/build.js uglify.defines.VERSION=string,3.0.0 uglify.defines.RELEASED=string,`date '+%Y-%m-%d'`
