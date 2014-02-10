b486eb6babb277e834b83b9764685cfec2e1581a
Eero Anttila <eero.anttila@reaktor.fi>
2014-02-10 22:32:22 +0200

    Update release scripts

--- playground.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-node src/lib/r-2.1.2.js -o site/playground-build.js
--- release.sh
+++ release.sh
@@ -11,11 +11,8 @@ compass compile -c config.rb
 rm build/*latest*
 [ "$version" != "" -a ! -f $BUILD_PREFIX-$version.js ] && rm build/*
 echo "Compressing js..."
-bundle() { node src/lib/r-2.1.2.js -o src/main/build.js uglify.defines.VERSION=string,$version uglify.defines.RELEASED=string,`date '+%Y-%m-%d'` $*; }
 update_version() { sed  -i '' -E "s/(\"version\".*:.*\").*(\".*)/\1$version\2/g" $@; }
-bundle out=$LATEST_JS_MIN
-echo "Updating playground..."
-node src/lib/r-2.1.2.js -o site/playground-build.js
+npm install
 echo "Compressing css..."
 java -jar yuicompressor-2.4.6.jar --type css src/main/jquery.continuousCalendar.css -o $LATEST_CSS_MIN
 #TODO create js documentation
