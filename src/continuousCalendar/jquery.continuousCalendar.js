d00953c140c5a18057285dcd181225da0cfe1c53
Eero Anttila <eea@iki.fi>
2016-02-17 14:12:33 +0200

    Build for version 5.0.1

--- src/continuousCalendar/jquery.continuousCalendar.js
+++ src/continuousCalendar/jquery.continuousCalendar.js
@@ -9,7 +9,7 @@ var RangeEvents = require('./RangeEvents')
 var SingleDateEvents = require('./SingleDateEvents')
 
 $.continuousCalendar = {
-  "version" : "5.0.0"
+  "version" : "5.0.1"
 }
 $.fn.continuousCalendar = function(options) {
   return this.each(function() { _continuousCalendar.call($(this), options) })
