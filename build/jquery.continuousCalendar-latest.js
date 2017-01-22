4ced61a500d8e44268cb198267d190bbac995163
Eero Anttila <eea@iki.fi>
2017-01-22 19:49:03 +0200

    Build bundle

--- build/jquery.continuousCalendar-latest.js
+++ build/jquery.continuousCalendar-latest.js
@@ -1892,7 +1892,9 @@ module.exports = function(container, options) {
     isRange:         false,
     startDate:       null,
     endDate:         null,
-    useIsoForInput:  false
+    useIsoForInput:  false,
+    initScrollBar: function() { },
+    executeCallback: function() { }
   }
   var params = extend(defaults, options)
   var locale = params.locale
