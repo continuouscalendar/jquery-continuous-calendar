708199b91edea4659d1ed3d920d93235430f93c4
Eero Anttila <eea@iki.fi>
2019-09-01 19:43:41 +0300

    Remove node_modules

--- node_modules/dateutils/src/global.js
+++
@@ -1,7 +0,0 @@
-window.DateFormat = require('./DateFormat')
-window.DateLocale = require('./DateLocale')
-window.DateParse = require('./DateParse')
-window.DateRange = require('./DateRange')
-window.DateTime = require('./DateTime')
-window.Duration = require('./Duration')
-
